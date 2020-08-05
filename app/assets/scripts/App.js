09a372a1251338bc48316ee4528199fcadca7fa6
Ewoksic <jasicn@gmail.com>
2020-08-05 20:05:22 +0200

    Style complite

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,18 @@
 import '../styles/styles.css'
 import 'lazysizes'
 
+const portfolioitems = document.querySelectorAll(".content__item")
+
+portfolioitems.forEach(portfolioitems => {
+    portfolioitems.addEventListener('mouseover', () => {
+        console.log(portfolioitems.childNodes[1].classList);
+        portfolioitems.childNodes[1].classList.add('img-darken');
+    })
+    portfolioitems.addEventListener('mouseout', () => {
+        console.log(portfolioitems.childNodes[1].classList);
+        portfolioitems.childNodes[1].classList.remove('img-darken');
+    })
+})
 
 
 if(module.hot){
